8e6818faf9c4fbfcbaff29e118a44bb358cb85bb
GitHub Action <action@github.com>
2023-11-23 12:11:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/kyay10/prettifykotlin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/kyay10/prettifykotlin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.kyay10.prettifykotlin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.kyay10.prettifykotlin.MyBundle
+import com.github.kyay10.prettifykotlin.services.MyProjectService
 import javax.swing.JButton
 
 
