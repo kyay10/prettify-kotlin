7ab0785417bb7db52b82e1f8e84f5948dbdda153
Youssef Shoaib <canonballt@gmail.com>
2025-03-19 07:57:06 +0000

    Add AutoLambda with prefix-handling

--- prettify-kotlin-annotation/src/commonMain/kotlin/io/github/kyay10/prettifykotlin/annotations.kt
+++ prettify-kotlin-annotation/src/commonMain/kotlin/io/github/kyay10/prettifykotlin/annotations.kt
@@ -3,3 +3,5 @@ package io.github.kyay10.prettifykotlin
 annotation class Pretty(val name: String)
 annotation class Prefix(val prefix: String, val suffix: String)
 annotation class Postfix(val suffix: String)
+@Target(AnnotationTarget.VALUE_PARAMETER)
+annotation class AutoLambda(val prefix: String, val arrow: String, val suffix: String)
--- src/main/kotlin/io/github/kyay10/prettifykotlin/PrettyFoldingBuilder.kt
+++ src/main/kotlin/io/github/kyay10/prettifykotlin/PrettyFoldingBuilder.kt
@@ -5,15 +5,18 @@ import com.intellij.lang.ASTNode
 import com.intellij.lang.folding.FoldingBuilderEx
 import com.intellij.lang.folding.FoldingDescriptor
 import com.intellij.openapi.editor.Document
-import com.intellij.openapi.editor.FoldingGroup
+import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.idea.references.mainReference
 import org.jetbrains.kotlin.idea.util.findAnnotation
+import org.jetbrains.kotlin.idea.util.isMultiline
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.psi.KtAnnotated
 import org.jetbrains.kotlin.psi.KtCallExpression
 import org.jetbrains.kotlin.psi.KtDotQualifiedExpression
+import org.jetbrains.kotlin.psi.KtFunction
 import org.jetbrains.kotlin.psi.KtLabelReferenceExpression
+import org.jetbrains.kotlin.psi.KtLambdaExpression
 import org.jetbrains.kotlin.psi.KtSimpleNameExpression
 import org.jetbrains.kotlin.psi.KtSuperExpression
 import org.jetbrains.kotlin.psi.KtThisExpression
@@ -26,6 +29,7 @@ import org.jetbrains.uast.toUElementOfType
 val PRETTY_FQNAME = FqName("io.github.kyay10.prettifykotlin.Pretty")
 val PREFIX_FQNAME = FqName("io.github.kyay10.prettifykotlin.Prefix")
 val POSTFIX_FQNAME = FqName("io.github.kyay10.prettifykotlin.Postfix")
+val AUTOLAMBDA_FQNAME = FqName("io.github.kyay10.prettifykotlin.AutoLambda")
 
 class PrettyFoldingBuilder : FoldingBuilderEx() {
   override fun buildFoldRegions(root: PsiElement, document: Document, quick: Boolean): Array<FoldingDescriptor> =
@@ -48,20 +52,55 @@ class PrettyFoldingBuilder : FoldingBuilderEx() {
 
         override fun visitCallExpression(expression: KtCallExpression) = impure {
           super.visitCallExpression(expression)
-
           val reference = expression.calleeExpression.bind().mainReference.bind().resolve()
           ensure(reference is KtAnnotated)
+          impure {
+            val annotation = reference.findAnnotation(PREFIX_FQNAME).bind()
+            val (prefix, suffix) = annotation.valueArguments.also { ensure(it.size == 2) }.map { it.constValue }
+            ensure(prefix is String && suffix is String)
 
-          val annotation = reference.findAnnotation(PREFIX_FQNAME).bind()
-          val (prefix, suffix) = annotation.valueArguments.also { ensure(it.size == 2) }.map { it.constValue }
-          ensure(prefix is String && suffix is String)
-
-          //val group = FoldingGroup.newGroup("brackets")
-          val (leftPar, rightPar) = expression.valueArgumentList.bind().run {
-            leftParenthesis.bind() to rightParenthesis.bind()
+            val (leftPar, rightPar) = expression.valueArgumentList.bind().run {
+              leftParenthesis.bind() to rightParenthesis.bind()
+            }
+            add(PrettyFoldingDescriptor(leftPar, prefix))
+            add(PrettyFoldingDescriptor(rightPar, suffix))
           }
-          add(PrettyFoldingDescriptor(leftPar, prefix))
-          add(PrettyFoldingDescriptor(rightPar, suffix))
+          impure {
+            ensure(reference is KtFunction)
+            val params = reference.valueParameters
+            val argToParam = expression.valueArguments.mapIndexed { index, arg ->
+              arg to (arg.name?.let { name -> params.find { it.name == name }.bind() } ?: params.getOrNull(index)
+                .bind())
+            }
+            argToParam.forEach { (arg, param) ->
+              val annotation = param.findAnnotation(AUTOLAMBDA_FQNAME) ?: return@forEach
+              val (prefix, arrow, suffix) = annotation.valueArguments.also { ensure(it.size == 3) }
+                .map { it.constValue }
+              ensure(prefix is String && arrow is String && suffix is String)
+              val lambda = (arg.getArgumentExpression() as? KtLambdaExpression ?: return@forEach).bind().functionLiteral
+              add(PrettyFoldingDescriptor(lambda.lBrace, prefix))
+              val lBraceWhitespace = lambda.lBrace.nextSibling
+              if (lBraceWhitespace.text.startsWith(" "))
+                add(PrettyFoldingDescriptor(lBraceWhitespace, "", range = TextRange(lBraceWhitespace.textRange.startOffset, lBraceWhitespace.textRange.startOffset + 1)))
+              lambda.arrow?.let {
+                add(PrettyFoldingDescriptor(it, arrow))
+                val afterArrowWhitespace = it.nextSibling
+                if (afterArrowWhitespace.text.startsWith(" "))
+                  add(PrettyFoldingDescriptor(afterArrowWhitespace, "", range = TextRange(afterArrowWhitespace.textRange.startOffset, afterArrowWhitespace.textRange.startOffset + 1)))
+                val beforeArrowWhitespace = it.prevSibling
+                if (beforeArrowWhitespace.text.endsWith(" "))
+                  add(PrettyFoldingDescriptor(beforeArrowWhitespace, "", range = TextRange(beforeArrowWhitespace.textRange.endOffset - 1, beforeArrowWhitespace.textRange.endOffset)))
+              }
+              lambda.rBrace?.let {
+                add(PrettyFoldingDescriptor(it, suffix))
+                if(!lambda.isMultiline()) {
+                  val rBraceWhitespace = it.prevSibling
+                  if (rBraceWhitespace.text.endsWith(" "))
+                    add(PrettyFoldingDescriptor(rBraceWhitespace, "", range = TextRange(rBraceWhitespace.textRange.endOffset - 1, rBraceWhitespace.textRange.endOffset)))
+                }
+              }
+            }
+          }
         }
 
         override fun visitDotQualifiedExpression(expression: KtDotQualifiedExpression) = impure {
